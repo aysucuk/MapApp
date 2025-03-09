c0d6e1f426f8475cac39d33b407b91e6a0bce603
aysucuk <aysusadixova@gmail.com>
2025-03-09 23:05:35 +0400

    Initial Commit

---
+++ MapApp/MapAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapAppApp.swift
+//  MapApp
+//
+//  Created by Aysu Sadıxova on 09.03.25.
+//
+
+import SwiftUI
+
+@main
+struct MapAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
