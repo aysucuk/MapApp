d024b58b6ec83e8eaf63f1b034a1dbcc45849eea
aysucuk <aysusadixova@gmail.com>
2025-03-13 23:09:38 +0400

    [Feature] Add data model and locations data service

---
+++ Models/Location.swift
@@ -0,0 +1,21 @@
+//
+//  Location.swift
+//  MapApp
+//
+//  Created by Aysu Sadıxova on 13.03.25.
+//
+
+import Foundation
+import MapKit
+
+struct Location {
+    
+    let name: String
+    let cityName: String
+    let coordinates: CLLocationCoordinate2D
+    let description: String
+    let imageNames: [String]
+    let link: String
+    
+    
+}
